b5e6a790bcdfa705ee9d588d74ff25d94fd3b15f
Sush1sui <jplovesraw@gmail.com>
2025-07-05 11:29:23 +0800

    feat: delete welcome message on member leave

---
+++ src/events/memberLeave.ts
@@ -0,0 +1,47 @@
+import { GuildMember, Message, TextChannel } from "discord.js";
+import { welcomeChannelId } from "../utils/helpers";
+
+
+export default {
+  name: 'guildMemberRemove',
+  async execute(member: GuildMember) {
+    if (member.user.bot) return;
+
+    try {
+      const welcomeChannel = member.guild.channels.cache.get(
+        welcomeChannelId
+      ) as TextChannel;
+
+      if (!welcomeChannel) throw new Error("Welcome channel not found");
+
+      let lastId;
+
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        const messages = await welcomeChannel.messages.fetch({
+          limit: 100,
+          before: lastId,
+        });
+
+        const messageToDelete = messages.find(
+          (m: Message) =>
+            m.author.id === member.client.user?.id &&
+            m.embeds[0]?.description?.includes(`<@${member.id}>`)
+        );
+
+        if (messageToDelete) {
+          await messageToDelete.delete();
+          break;
+        }
+
+        if (messages.size === 0) {
+          break;
+        }
+
+        lastId = messages.last()!.id;
+      }
+    } catch (error) {
+      console.error("Error sending leave message:", error);
+    }
+  },
+};
